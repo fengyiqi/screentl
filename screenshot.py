08230560d1b9913fac66078e2d592ee8b4fee8f7
fengyiqi <yiqi.feng@hotmail.com>
2021-01-24 12:23:40 +0100

    upload

---
+++ screenshot.py
@@ -0,0 +1,11 @@
+from screentl.utils import screenshot
+import datetime
+
+
+today = datetime.date.today().strftime('%Y-%m-%d')
+
+# argument 'folder' can be replaced by whatever you want. For example, you can name it
+# by your project name. In this way you could organize your screenshots in the folders
+# categorised by your work.
+# interval: how many seconds will you capture the screen.
+screenshot(folder='today', interval=25)
